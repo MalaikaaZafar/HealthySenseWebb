752fe4e5b6e26a90d448cc17734b8994e0749355
TalalHabib123 <talalhabib101@gmail.com>
2023-12-13 11:51:27 +0500

    Abdullahs contribution

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -15,7 +15,7 @@ app.get('/', (res, req)=>{
     res.send('Hello to MERN API');
 })
 
-const PORT =process.env.PORT || 5000;
+const PORT =process.env.PORT || 3000;
 
 mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true,})
     .then(()=> app.listen(PORT, ()=> console.log('Server Running on port:', PORT)))
