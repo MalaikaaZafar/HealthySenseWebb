d082575671d06993c292ac937622dc3f0b566981
TalalHabib123 <talalhabib101@gmail.com>
2023-12-15 16:45:54 +0500

    Misc changes to schema

---
+++ BackEnd/models/Diagnosis.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const diagnosisSchema = new mongoose.Schema({
+    date: { type: Date, required: true },
+    description: { type: String, required: true },
+    type: { type: String, required: true },
+    doctorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor', required: true },
+    patientId: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient', required: true },
+    prescription: [{ type: String, required: true }],
+    tests: [{ type: String, required: true }], 
+});
+
+const Diagnosis = mongoose.model('Diagnosis', diagnosisSchema);
+
+module.exports = Diagnosis;
