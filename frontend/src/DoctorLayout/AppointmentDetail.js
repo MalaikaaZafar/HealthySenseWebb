919d8db5ee344f35cf477c0dd8449c3a2b4b529d
TalalHabib123 <talalhabib101@gmail.com>
2023-12-22 10:35:37 +0500

    Merging Branch Changes

--- frontend/src/DoctorLayout/AppointmentDetail.js
+++ frontend/src/DoctorLayout/AppointmentDetail.js
@@ -7,29 +7,29 @@ function AppointmentDetail() {
   return (
     <div className="appointmentDetailsScreen">
       <div className="ScreenBody">
-       <div className="half">
-       <div className="patientCard">
-          <div className="patientImage">
-            <img src="https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg"></img>
+        <div className="half">
+          <div className="patientCard">
+            <div className="patientImage">
+              <img src="https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg"></img>
+            </div>
+            <div className="patientDetails">
+              <div className="docName"><p>Patient name</p></div>
+              <div className="docSpeciality"><p>Age: 5</p></div>
+              <div className="docExperience"><p>Phone: 12345678910</p></div>
+            </div>
           </div>
-          <div className="patientDetails">
-            <div className="docName"><p>Patient name</p></div>
-            <div className="docSpeciality"><p>Age: 5</p></div>
-            <div className="docExperience"><p>Phone: 12345678910</p></div>
-          </div>
-        </div>
-        <DetailComponent/>
-        
+          <DetailComponent />
+
         </div>
 
-      <div className="half">
-      <div className='appointmentBtns'>
-            <Button variant="contained" color="primary" style={{background: '#2854c3', marginBottom:'10px', width: '50%'}}>Reschedule Appointment</Button>
-            <Button variant="contained" color="primary" style={{background: '#2854c3', width: '50%'}}>Cancel Appointment</Button>
-        </div>
+        <div className="half">
+          <div className='appointmentBtns'>
+            <Button variant="contained" color="primary" style={{ background: '#2854c3', marginBottom: '10px', width: '50%' }}>Reschedule Appointment</Button>
+            <Button variant="contained" color="primary" style={{ background: '#2854c3', width: '50%' }}>Cancel Appointment</Button>
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
