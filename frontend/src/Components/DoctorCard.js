4e3984c71c8c049fdba79746f97397660c277fee
M-Abdullah03 <m18ab03@gmail.com>
2023-12-26 19:33:13 +0500

    Completed favorites, error validation in search and favorites

--- frontend/src/Components/DoctorCard.js
+++ frontend/src/Components/DoctorCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Card from '@mui/material/Card';
 import Avatar from '@mui/material/Avatar';
 import FeeIcon from '@mui/icons-material/AttachMoneyOutlined';
@@ -6,20 +6,92 @@ import { Button, Divider } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import axios from 'axios';
 
 import './DoctorCard.css';
+import { add } from 'date-fns';
+import useUserStore from '../stores/userStore';
 
-function DoctorCard({ user, buttons }) {
+const removeFavorite = async (doctorId) => {
+    try {
+        const token = localStorage.getItem('token');
+        const response = await axios.delete(`http://localhost:3000/favorites/${doctorId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        if (response.status === 200) {
+            return true;
+        }
+        return false;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+const addFavorite = async (doctorId) => {
+    try {
+        console.log(doctorId);
+        const token = localStorage.getItem('token');
+        const response = await axios.post(`http://localhost:3000/favorites/${doctorId}`, {}, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        console.log(response);
+        return response;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+
+function DoctorCard({ user, buttons, onFavChanged }) {
     const [favourite, setFavourite] = React.useState(false);
+    const { user: loggedUser, updateUser } = useUserStore();
+    const [actionCompleted, setActionCompleted] = React.useState(true);
     const goToUserPage = () => {
         //Navigate to user page
         alert("Navigate to user page");
     }
-    const toggleFavourite = (e) => {
+    const toggleFavourite = async (e) => {
         e.stopPropagation();
-        // Add to favourite
+        setActionCompleted(false);
         setFavourite((prevFavourite) => !prevFavourite);
+        if (favourite) {
+            const res = await removeFavorite(user.user._id);
+            if (!res) {
+
+                setActionCompleted(true);
+                alert("Something went wrong");
+                setFavourite((prevFavourite) => !prevFavourite);
+                return;
+            }
+        }
+        else {
+            const res = await addFavorite(user.user._id);
+            if (!res) {
+                alert("Something went wrong");
+                setActionCompleted(true);
+                setFavourite((prevFavourite) => !prevFavourite);
+                return;
+            }
+        }
+        if (onFavChanged) {
+            onFavChanged();
+        }
+        await updateUser();
+        setActionCompleted(true);
     }
+
+    useEffect(() => {
+        updateUser();
+        if (loggedUser.favoriteDoctors?.includes(user.user._id)) {
+            setFavourite(true);
+        }
+    }, [user]);
     return (
         <Card
             variant="outlined"
@@ -46,7 +118,10 @@ function DoctorCard({ user, buttons }) {
                 <div className="docDetailTop" style={{}}>
                     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                         <div className="docName">Dr. {user.user.name}</div>
-                        <IconButton onClick={toggleFavourite} sx={{ p: 0, color: '#2854C3' }}>
+                        <IconButton
+                            onClick={toggleFavourite}
+                            disabled={!actionCompleted}
+                            sx={{ p: 0, color: '#2854C3' }}>
                             {favourite ? <FavoriteIcon /> : <FavoriteBorderIcon />}
                         </IconButton>
                     </div>
@@ -67,9 +142,9 @@ function DoctorCard({ user, buttons }) {
             </div>
             <div className='location-fee'>
                 <div className='location'>
-                    <p ><FeeIcon sx={{}} />{user.fee} Rs</p>
-                </div>
-                <div className='user-card-Office'>
+                    <p style={{ display: 'flex', alignItems: 'center' }}>
+                        <span><FeeIcon sx={{}} /></span>
+                        <span>{user.fee} Rs</span></p>
                 </div>
             </div>
             <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
