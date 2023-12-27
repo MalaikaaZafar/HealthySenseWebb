fab534228d342acf2ece0a8ceb0577b6f3f5ae30
moizBhai97 <moizrootsdha@gmail.com>
2023-12-28 04:57:25 +0500

    responsiveness

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,6 @@ import DoctorReview from './pages/patient/DoctorReview.js';
 import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@emotion/react';
 import theme from './Theme.js';
-import ViewAllPatients from './adminlayout/ViewAllPatients.js';
 
 function App() {
   return (
