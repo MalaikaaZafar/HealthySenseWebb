4a909f172c68d0f945bb1e9e63fe037eb0d67524
moizBhai97 <moizrootsdha@gmail.com>
2023-12-27 15:40:27 +0500

    moiz branch cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,8 @@ import AppointmentSlots from './DoctorLayout/appointment_slots/AppointmentSlots.
 import RescheduleAppointment from './DoctorLayout/reschedule_appointment/RescheduleAppointment.js';
 import AppointmentDetail from './DoctorLayout/view_appointment/AppointmentDetail.js';
 
-import ViewAllDoctors from './adminlayout/ViewAllDoctors.js';
-import ViewAllPatients from './adminlayout/ViewAllPatients.js';
+import ViewAllDoctors from './adminLayout/ViewAllDoctors.js';
+import ViewAllPatients from './adminLayout/ViewAllPatients.js';
 
 import { RescheduleAppointment as ReschedulePatient } from './patientlayout/reschedule_appointment/RescheduleAppointment.js';
 import { AppointmentList as AppointmentListPatient } from './patientlayout/view_appointment_list/ViewAppointmentList.js';
@@ -15,61 +15,64 @@ import { AppointmentDetail as ApptDetail } from './patientlayout/view_appointmen
 import { BookAppointment } from './patientlayout/book_appointment/BookAppointment.js';
 
 import { Routes, Route } from 'react-router-dom';
-import Search from './pages/Search.js';
-import Favorites from './pages/Favorites.js';
-import AdminDoctor from './pages/AdminDoctor.js';
-import AdminPatient from './pages/AdminPatient.js';
-import AdminActivity from './pages/AdminActivity.js';
+import Search from './pages/user/Search.js';
+import Favorites from './pages/patient/Favorites.js';
+import AdminDoctor from './pages/admin/AdminDoctor.js';
+import AdminPatient from './pages/admin/AdminPatient.js';
+import AdminActivity from './pages/admin/AdminActivity.js';
 
-import Login from './pages/Login.js';
-import Signup from './pages/Signup.js';
-import RegisterDoctor from './pages/RegisterDoctor.js';
-import DoctorDeatils from './pages/DoctorDetails.js';
-import PatientDetails from './pages/PatientDetails.js';
-import DoctorReview from './pages/DoctorReview.js';
+import Login from './pages/auth/Login.js';
+import Signup from './pages/auth/Signup.js';
+import RegisterDoctor from './pages/auth/Register.js';
+import DoctorDeatil from './pages/user/DoctorDetail.js';
+import PatientDetail from './pages/doctor/PatientDetail.js';
+import DoctorReview from './pages/patient/DoctorReview.js';
+
+import { ThemeProvider } from '@emotion/react';
+import theme from './Theme.js';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/register" element={<RegisterDoctor />} />
 
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/doctor/register" element={<RegisterDoctor />} />
-        <Route path='/doctor-detail' element={<DoctorDeatils />} />
-        <Route path='/patient-detail' element={<PatientDetails />} />
-
-        <Route path="/admin" element={<NavBar />}>
-          <Route index element={<ViewAllPatients />} />
-          <Route path="doctors" element={<AdminDoctor />} />
-          <Route path='patients' element={<AdminPatient />} />
-          <Route path='activity' element={<AdminActivity />} />
-        </Route>
-        <Route path="/doctor" element={<NavBar />}>
-          <Route index element={<CustomTabPanel />} />
-          <Route path="appointments/:id" element={<AppointmentDetail />} />
-          <Route path="appointments" element={<CustomTabPanel />} />
-          <Route path="appointments/cancel/:id" element={<CancelAppointment />} />
-          <Route path="appointmentSlots" element={<AppointmentSlots />} />
-          <Route path="appointments/reschedule/:id" element={<RescheduleAppointment />} />
-        </Route>
-
-        <Route path='/patient' element={<NavBar />}>
-          <Route index element={<Search />} />
-          <Route index element={<AppointmentListPatient />} />
-          <Route path='appointments' element={<AppointmentListPatient />} />
-          <Route path='appointments/:id' element={<ApptDetail />} />
-          <Route path='appointments/reschedule/:id' element={<ReschedulePatient />} />
-          <Route path='appointments/cancel/:id' element={<CancelPatientAppt />} />
-          <Route path='book-appointment/:id' element={<BookAppointment />} />
-          <Route path='review' element={<DoctorReview />} />
-        </Route>
-        <Route path="/" element={<NavBar />}>
-          <Route index element={<Search />} />
-          <Route path='favorites' element={<Favorites />} />
-        </Route>
-      </Routes>
-    </div >
+          <Route path="/admin" element={<NavBar />}>
+            <Route index element={<ViewAllPatients />} />
+            <Route path="doctors" element={<AdminDoctor />} />
+            <Route path='patients' element={<AdminPatient />} />
+            <Route path='activity' element={<AdminActivity />} />
+          </Route>
+          <Route path="/doctor" element={<NavBar />}>
+            <Route index element={<CustomTabPanel />} />
+            <Route path="appointments/:id" element={<AppointmentDetail />} />
+            <Route path="appointments" element={<CustomTabPanel />} />
+            <Route path="appointments/cancel/:id" element={<CancelAppointment />} />
+            <Route path="appointmentSlots" element={<AppointmentSlots />} />
+            <Route path="appointments/reschedule/:id" element={<RescheduleAppointment />} />
+            <Route path='patient-detail' element={<PatientDetail />} />
+          </Route>
+          <Route path='/patient' element={<NavBar />}>
+            <Route index element={<Search />} />
+            <Route index element={<AppointmentListPatient />} />
+            <Route path='appointments' element={<AppointmentListPatient />} />
+            <Route path='appointments/:id' element={<ApptDetail />} />
+            <Route path='appointments/reschedule/:id' element={<ReschedulePatient />} />
+            <Route path='appointments/cancel/:id' element={<CancelPatientAppt />} />
+            <Route path='book-appointment/:id' element={<BookAppointment />} />
+            <Route path='review' element={<DoctorReview />} />
+            <Route path='favorites' element={<Favorites />} />
+          </Route>
+          <Route path="/" element={<NavBar />}>
+            <Route path='doctor-detail' element={<DoctorDeatil />} />
+            <Route index element={<Search />} />
+          </Route>
+        </Routes>
+      </div >
+    </ThemeProvider>
   );
 }
 
--- frontend/src/pages/admin/AdminDoctor.js
+++ frontend/src/pages/admin/AdminDoctor.js
@@ -3,37 +3,34 @@ import { Box, Button, TextField, IconButton } from '@mui/material';
 import TextRotationNoneIcon from '@mui/icons-material/TextRotationNone';
 import InputAdornment from '@mui/material/InputAdornment';
 import SearchIcon from '@mui/icons-material/Search';
-import DoctorCard from '../components/DoctorCard';
-import searchDoctors from '../services/searchDoctors';
-import FilterPopover from '../components/FilterPopover';
+import DoctorCard from '../../components/DoctorCard';
+import FilterPopover from '../../components/FilterPopover';
 import './Search.css';
-import styles from '../styles/searchStyles';
-import useUserStore from '../stores/userStore';
-import axios from 'axios';
+import styles from '../../styles/searchStyles';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import SearchErrorMessage from '../components/SearchErrorMessage';
+import SearchErrorMessage from '../../components/SearchErrorMessage';
+import ViewAlldoctors from '../../adminLayout/ViewAllDoctors';
+import searchDoctors from '../../services/admin/searchDoctor';
 
 
-const Search = () => {
-
-    const [selectedButton, setSelectedButton] = React.useState('A-Z');
+const AdminDoctor = () => {
     const [sortDirection, setSortDirection] = React.useState('asc');
     const [searchText, setSearchText] = React.useState('');
     const [doctors, setDoctors] = React.useState([]);
     const [specialtyFilter, setSpecialtyFilter] = React.useState('');
-    const [minRating, setMinRating] = React.useState(0);
-    const { updateUser } = useUserStore();
     const [notFound, setNotFound] = useState(false);
     const [hasMore, setHasMore] = useState(true);
     const [actionCompleted, setActionCompleted] = React.useState(true);
     const [error, setError] = React.useState(false);
+    const [searched, setSearched] = React.useState(false);
     let direction = 'asc';
     // ...
 
     const fetchMoreData = async () => {
+
         console.log("fetching more data")
         const skip = doctors.length;
-        let moreDoctors = await searchDoctors(searchText, selectedButton, direction, specialtyFilter, minRating, skip);
+        let moreDoctors = await searchDoctors(searchText, direction, specialtyFilter, skip);
         if (moreDoctors.length === 0) {
             setHasMore(false);
             return;
@@ -41,27 +38,12 @@ const Search = () => {
         setDoctors(doctors.concat(moreDoctors));
     };
 
-    useEffect(() => {
-        updateUser();
-    }, []);
 
-    const handleButtonClick = (value) => {
-        setSelectedButton((prevSelected) => {
-            if (prevSelected === value) {
-                return prevSelected;
-            }
-            searchPressed();
-            return value;
-        });
-    };
 
     const handleSpecialtyFilter = (value) => {
         setSpecialtyFilter(value);
     };
 
-    const handleMinRating = (value) => {
-        setMinRating(value);
-    };
 
     const handleSearch = (value) => {
         setSearchText(value);
@@ -81,8 +63,7 @@ const Search = () => {
         //     alert('Please enter a search query');
         // }
         setActionCompleted(false);
-        console.log("searching", direction);
-        const result = await searchDoctors(searchText, selectedButton, direction, specialtyFilter, minRating, 0);
+        const result = await searchDoctors(searchText, direction, specialtyFilter, 0);
         console.log(result);
         if (result == -1) {
             setError(true);
@@ -100,6 +81,7 @@ const Search = () => {
         setActionCompleted(true);
         setError(false);
         setHasMore(true);
+        setSearched(true);
 
 
     };
@@ -129,18 +111,9 @@ const Search = () => {
                                 ),
                             }}
                         />
-                        <FilterPopover handleFilterChange={handleSpecialtyFilter} handleMinRating={handleMinRating} specialty={specialtyFilter} minRating={minRating} onApply={searchPressed} />
+                        <FilterPopover handleFilterChange={handleSpecialtyFilter} specialty={specialtyFilter} onApply={searchPressed} />
                     </Box>
                     <Box sx={styles.buttonGroup}>
-                        <Button variant='outlined' color="primary" onClick={() => handleButtonClick('A-Z')} sx={selectedButton === 'A-Z' ? { ...styles.button, backgroundColor: '#2854C3', color: '#fff' } : styles.button}>
-                            A-Z
-                        </Button>
-                        <Button variant='outlined' color="primary" onClick={() => handleButtonClick('Price')} sx={selectedButton === 'Price' ? { ...styles.button, backgroundColor: '#2854C3', color: '#fff' } : styles.button}>
-                            Price
-                        </Button>
-                        <Button variant='outlined' color="primary" onClick={() => handleButtonClick('Rating')} sx={selectedButton === 'Rating' ? { ...styles.button, backgroundColor: '#2854C3', color: '#fff' } : styles.button}>
-                            Rating
-                        </Button>
                         <IconButton
                             aria-label="sort direction"
                             onClick={toggleSortDirection}
@@ -168,17 +141,17 @@ const Search = () => {
                 <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', p: 2, justifyContent: 'center', alignItems: 'flex-start', width: '100%' }}>
                     {doctors?.length > 0 && doctors.map((doctor) => {
                         return (
-                            <DoctorCard user={doctor} buttons={true} />
+                            <DoctorCard user={doctor} />
                         );
                     })}
                 </Box>
             </InfiniteScroll>
                 :
-                <SearchErrorMessage notFound={notFound} error={error} />
+                !notFound && !searched && !error ? <ViewAlldoctors /> : <SearchErrorMessage notFound={notFound} error={error} />
             }
         </div >
     );
 };
 
-export default Search;
+export default AdminDoctor;
 
