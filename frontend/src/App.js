a936111500205dc8a9397a690a7bf84fbd9907c9
TalalHabib123 <talalhabib101@gmail.com>
2024-01-02 01:29:36 +0500

    gfgf

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -79,7 +79,7 @@ function App() {
               <Route path='appointments/reschedule/:id' element={<ReschedulePatient />} />
               <Route path='appointments/cancel/:id' element={<CancelPatientAppt />} />
               <Route path='book-appointment/:id' element={<BookAppointment />} />
-              <Route path='reports/:id' element={<AllReports />} />
+              <Route path='reports' element={<AllReports />} />
               <Route path='payment/:appid' element={<Payment />} />
               <Route path='your-report/:diagid' element={<ViewReportPatient />} />
               <Route path='review' element={<DoctorReview />} />
